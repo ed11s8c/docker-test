1d05b7ed6ccd5ff4cbc055973ff0a6c50348aa8e
Steven Cooney <dev.sjc@gmail.com>
2017-05-21 15:35:29 +0100

    Update script

--- ci-resources/deploy.sh
+++ ci-resources/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -x #echo on
 
+envsubst < ci-resources/tag-template.json > tag-body.json
 ls -al
+cat tag-body.json
+curl -X POST -H "Content-Type: application/json" -d ''{"build": true}'' "https://registry.hub.docker.com/u/stevencooney/docker-test/trigger/$DOCKER_HUB_TOKEN/"
+curl -X POST -H "Content-Type: application/json" -d '@./tag-body.json' "https://registry.hub.docker.com/u/stevencooney/docker-test/trigger/$DOCKER_HUB_TOKEN/"
