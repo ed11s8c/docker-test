91d2d0b21f66320e4a65e7190a2a3fdcff4abe38
Steven Cooney <dev.sjc@gmail.com>
2017-05-21 14:17:08 +0100

    update

--- ci-resources/release.sh
+++ ci-resources/release.sh
@@ -3,5 +3,5 @@ set -x #echo on
 
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
   curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/stevencooney/docker-test/trigger/$DOCKER_HUB_TOKEN/
-  curl -H "Content-Type: application/json" --data '@./ci-resources/tag-body.json' -X POST https://registry.hub.docker.com/u/stevencooney/docker-test/trigger/$DOCKER_HUB_TOKEN/
+  curl -H "Content-Type: application/json" --data '@./tag-body.json' -X POST https://registry.hub.docker.com/u/stevencooney/docker-test/trigger/$DOCKER_HUB_TOKEN/
 fi
